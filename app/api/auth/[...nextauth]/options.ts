adef49d2ed3a301eed437db3581b6c7c085cc9df
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-04-09 13:56:06 +0200

    tr

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -114,3 +114,247 @@ export const options: NextAuthOptions = {
     },
   },
 };
+
+// // pages/api/auth/[...nextauth].js
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import FacebookProvider from "next-auth/providers/facebook";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import User from "@/models/User";
+// import connectToDatabase from "@/utils/connectDb";
+
+// export default NextAuth({
+//   pages: {
+//     // customized pages
+//     signIn: "/login",
+//     error: "/error", // Custom error page URL
+//     // signOut: '/auth/signout',
+//     // verifyRequest: '/auth/verify-request',
+//     // newUser: '/auth/new-user'
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "email", type: "email" },
+//         // username: { label: "Username", type: "text" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       //   is called when the user submits the login form. It receives the credentials as a parameter and should return a user object if the credentials are valid, or null if they are not. AFTER this method, the signIn method is called
+//       authorize: async (credentials) => {
+//         console.log("🚀 ~~~~~~~~~authorize~~~~~~~~~");
+//         console.log("🚀 ~authorize credentials:", credentials);
+//         await connectToDatabase();
+//         try {
+//           const user = await User.findOne({ email: credentials.email });
+//           console.log("🚀 ~authorize user:", user);
+
+//           if (!user) return Promise.resolve(null);
+
+//           const isValid = await bcrypt.compare(
+//             credentials.password,
+//             user.password
+//           );
+//           console.log("🚀 ~authorize isValid:", isValid);
+
+//           if (!isValid) return Promise.resolve(null);
+
+//           return Promise.resolve(user);
+//         } catch (error) {
+//           console.error("Authorise Error fetching user:", error.message);
+//           return Promise.resolve(null);
+//         }
+//       },
+//     }),
+//     //   GOOGLE
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // GITHUB
+//     GithubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//     }),
+//     // FACEBOOK
+//     FacebookProvider({
+//       clientId: process.env.FACEBOOK_CLIENT_ID,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+//     }),
+//     // Add more providers as needed
+//   ],
+//   // Add custom configurations below
+//   database: process.env.DATABASE_URL,
+//   session: {
+//     strategy: "jwt",
+//     maxAge: 30 * 24 * 60 * 60, // 30 days
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+//   callbacks: {
+//     /**
+//      *
+//      * When it's used: After a user is verified but before they are signed in.
+//      * Purpose: Control whether or not to allow the user to sign in.
+//      * Parameters: user, account, profile, and email.
+//      * Returns: true (allow sign-in), false (deny sign-in), or a URL (redirect to the URL).
+//      * Parameters: (user, account, profile, email)
+//      *
+//      */
+//     async signIn(user, account, profile) {
+//       console.log("🚀 ~~~~~~~~~signIn~~~~~~~~~");
+//       console.log("🚀 ~ SIGNIN :user:", user);
+//       console.log("🚀 ~ SIGNIN :account:", account);
+//       console.log("🚀 ~ SIGNIN :profile:", profile);
+
+//       let email = "";
+
+//       // Handling Credentials Provider
+//       if (user?.account?.provider === "credentials") {
+//         return true;
+//       } else if (user.account.provider === "github") {
+//         email = user.account.email; // github returns this user object only
+
+//         // If email is not available, fetch it from GitHub API
+//         if (!email) {
+//           const res = await fetch("https://api.github.com/user/emails", {
+//             headers: {
+//               Authorization: `token ${user.account.access_token}`,
+//             },
+//           });
+//           const emails = await res.json();
+//           console.log("🚀 ~SIGNIN emails from GITHUB:", emails);
+
+//           if (!emails || emails.length === 0) {
+//             return false; // No email found, deny access
+//           }
+
+//           // Find the primary email
+//           const primaryEmail = emails.find((e) => e.primary);
+//           email = primaryEmail?.email;
+//           console.log("🚀 ~ email finally is:", email);
+//         }
+//       } // Handling Google Provider
+//       else if (user.account.provider === "google") {
+//         email = user.user.email;
+//       }
+
+//       console.log("🚀 ~SIGNIN email:", email);
+
+//       if (email) {
+//         await connectToDatabase();
+//         let dbUser = await User.findOne({ email });
+//         console.log("🚀 ~SIGNIN dbUser:", dbUser);
+//         if (!dbUser) {
+//           // If user does not exist, create them
+//           const newUser = {
+//             email,
+//             verified: true, // Set verified to true for OAuth providers
+//             username: email, // there is no username so use the email as username
+//             password: null, // there is no pass from user so set it to null
+//             provider: user.account.provider,
+//           };
+
+//           console.log("🚀 ~SIGNIN newUser:", newUser);
+//           dbUser = await User.create(newUser);
+//           console.log("🚀 ~ SIGNIN dbUser:", dbUser);
+//         }
+
+//         // store user data from db so they are accessible in jwt callback and then could be available in session callback.
+//         user.account.userData = dbUser;
+//         // Additional checks or actions here
+//         return true; // Allow sign-in
+//       }
+//       return false; // Prevent sign-in
+//     },
+
+//     /**
+//      *
+//      * When it's used: Whenever NextAuth.js has to redirect a user (e.g., on sign-in or sign-out).
+//      * Purpose: Control the URLs users are redirected to.
+//      * Parameters: url and baseUrl.
+//      * Returns: A URL.
+//      *
+//      */
+//     async redirect(address, baseAddress) {
+//       console.log("🚀 ~~~~~~~~~redirect~~~~~~~~~");
+//       console.log("🚀 ~redirect address, baseAddress:", address, baseAddress);
+
+//       const { url, baseUrl } = address;
+
+//       if (url.includes("/login") || url.includes("/register")) {
+//         return baseUrl + "/dashboard";
+//       }
+
+//       return url.startsWith(baseUrl) ? url : baseUrl;
+//     },
+//     /**
+//      * SESSION
+//      * When it's triggered: Whenever a session is checked/updated.
+//      * Purpose: Control the session object.
+//      * Returns: The session object.
+//      */
+//     async session({ session, token, user }) {
+//       console.log("🚀 ~~~~~~~~~session~~~~~~~~~");
+//       console.log("🚀 ~ session:", session);
+//       console.log("🚀 ~ user:", user);
+//       console.log("🚀 ~ token:", token);
+
+//       session.customAccessToken = token.customAccessToken;
+
+//       return { ...session, token };
+//     },
+//     /**
+//      * JWT
+//      * is triggered whenever a JSON Web Token (JWT) is created or updated, which happens at sign-in or whenever a session is accessed on the client
+//      * When it's used: When signing the JWT or when validating/updating the JWT.
+//      * Purpose: Control the properties of the JWT.
+//      * Parameters: token, user, account, profile
+//      * Returns: The token object.
+//      */
+//     async jwt({ token, user, account, profile }) {
+//       console.log("🚀 ~~~~~~~~~jwt~~~~~~~~~");
+//       console.log("🚀 ~ token:", token);
+//       console.log("🚀 ~ user:", user);
+//       console.log("🚀 ~ account:", account);
+//       console.log("🚀 ~ profile:", profile);
+
+//       // if user signs in with OATH
+//       if (account?.userData) {
+//         const userObject = account.userData.toObject();
+//         const safeUser = omit(userObject, ["password", "verificationToken"]);
+//         token.user = safeUser;
+//         console.log("🚀 ~ safeUser:", safeUser);
+
+//         token.customAccessToken = jwt.sign(
+//           { userId: account?.userData._id },
+//           process.env.NEXTAUTH_SECRET
+//         );
+
+//         return token;
+//       }
+
+//       if (user) {
+//         const userObject = user.toObject();
+//         const safeUser = omit(userObject, ["password", "verificationToken"]);
+//         token.user = safeUser;
+
+//         token.customAccessToken = jwt.sign(
+//           { userId: user._id },
+//           process.env.NEXTAUTH_SECRET
+//         );
+//       }
+
+//       return token;
+//     },
+//   },
+// });
+
+// // Utility function to omit specific properties from an object
+// function omit(obj, keys) {
+//   const result = { ...obj };
+//   keys.forEach((key) => delete result[key]);
+//   return result;
+// }
