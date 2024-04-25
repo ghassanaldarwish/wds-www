7f69854364b259dc625609a96f57a619631d5ad5
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-04-25 18:56:53 +0200

    wd

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="h-[70px] z-10 fixed top-0 w-screen flex items-center bg-background border-b">
+    <nav className="h-[60px] z-30 fixed top-0 w-screen flex items-center bg-background border-b">
       <div className="container flex justify-between">
         <div className="text-3xl font-bold">WebDrei</div>
         <div className="flex items-center gap-12">
