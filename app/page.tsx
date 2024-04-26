b9d47852f132774dd47518931d11cccf9801d89a
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-04-27 00:23:36 +0200

    opt hero and nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Benefits from "@/components/benefits";
-import CiCd from "@/components/ci-cd";
+
 import Feature from "@/components/feature";
 import Hero from "@/components/hero";
-
+import dynamic from "next/dynamic";
+const CiCd = dynamic(() => import("@/components/ci-cd"), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
